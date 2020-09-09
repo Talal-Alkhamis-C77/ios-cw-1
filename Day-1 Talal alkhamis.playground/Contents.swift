9aa99f57ac4fab75bb77deb05bc812fd9db1d607
Talal-Alkhamis-C77 <70804941+Talal-Alkhamis-C77@users.noreply.github.com>
2020-09-09 20:11:01 +0300

    DONE

---
+++ Day-1 Talal alkhamis.playground/Contents.swift
@@ -0,0 +1,19 @@
+var name = "Talal"
+var age = 15
+var hobby1 = "Photography"
+var hobby2 = "Coding"
+var height = 167
+print("my name is \(name) my age is: \(age)")
+var friendname = "Ali"
+var friendage = 18
+var friendhobby1 = "Hourse riding"
+var friendhobby2 = "Coding"
+var friendhieght = 170
+ var agediffrence = age - friendage
+age > friendage
+var diffrence = hobby1 == friendhobby2 || friendhobby1 == hobby2 || hobby1 == friendhobby1 || hobby2 == friendhobby2
+
+
+
+
+
